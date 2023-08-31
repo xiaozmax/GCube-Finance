141347a98883b1c0ee59df724a7423ae13722e9d
Makads <98026146+Makads@users.noreply.github.com>
2023-08-31 22:21:22 +0800

    Update app.php

--- app/app.php
+++ app/app.php
@@ -1,10 +1,4 @@
 <?php
-/*
- * @ https://EasyToYou.eu - IonCube v11 Decoder Online
- * @ PHP 7.2 & 7.3
- * @ Decoder version: 1.0.6
- * @ Release: 10/08/2022
- */
 
 think\facade\Lang::load(APP_PATH . "common/lang/zh-cn.php");
 if (!defined("UPLOAD_PATH")) {
