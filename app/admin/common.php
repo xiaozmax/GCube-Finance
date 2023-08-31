b57ee10ab9aaae4c5f96103768a856cd7bcf5d66
Makads <98026146+Makads@users.noreply.github.com>
2023-08-31 22:31:53 +0800

    Update common.php

--- app/admin/common.php
+++ app/admin/common.php
@@ -1,10 +1,4 @@
 <?php
-/*
- * @ https://EasyToYou.eu - IonCube v11 Decoder Online
- * @ PHP 7.2 & 7.3
- * @ Decoder version: 1.0.6
- * @ Release: 10/08/2022
- */
 
 Lang::load(APP_PATH . "admin/lang/zh-cn.php");
 function get_modules($type = "plugin")
