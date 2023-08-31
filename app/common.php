63d9b884679a2e9f6584cb5f43a3740ebd2c045c
Makads <98026146+Makads@users.noreply.github.com>
2023-08-31 22:23:28 +0800

    Update common.php

--- app/common.php
+++ app/common.php
@@ -1,10 +1,4 @@
 <?php
-/*
- * @ https://EasyToYou.eu - IonCube v11 Decoder Online
- * @ PHP 7.2 & 7.3
- * @ Decoder version: 1.0.6
- * @ Release: 10/08/2022
- */
 
 require_once "zjmf.php";
 if (file_exists(CMF_ROOT . "app/res/common.php")) {
